b8fab3ecd0485c710c24417234d66454314feb92
github_DASorescu <srsdln97@gmail.com>
2022-07-11 15:45:31 +0200

    bonus-added

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ console.log('vue ok');
 const app = new Vue({
     el: '#mydiv',
     data: {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      image: 'https://i.picsum.photos/id/237/3500/2095.jpg?hmac=y2n_cflHFKpQwLOL1SSCtVDqL8NmOnBzEW7LYKZ-z_o'
+
     }
-  })
+  });
